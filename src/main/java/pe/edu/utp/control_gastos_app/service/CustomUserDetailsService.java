0738a35bb7be8a353b12fbd2c3b2e029c2c83a4d
Wilson Arenas Mendoza <arenaswilson7@gmail.com>
2026-05-02 13:21:06 -0500

    Se hizo login y register

---
+++ src/main/java/pe/edu/utp/control_gastos_app/service/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package pe.edu.utp.control_gastos_app.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pe.edu.utp.control_gastos_app.model.User;
+import pe.edu.utp.control_gastos_app.repository.UserRepository;
+
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles().stream()
+                        .map(r -> new SimpleGrantedAuthority(r.getName()))
+                        .collect(Collectors.toList())
+        );
+    }
+}
