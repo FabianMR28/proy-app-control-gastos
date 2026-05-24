7ab63ed6290997366c589fe28e0a70b58500ed85
Joseph Montoya <j.jhoel@outlook.es>
2026-05-23 23:31:46 -0500

    feat: Actualizando ReportController

--- src/main/java/pe/edu/utp/control_gastos_app/controller/ReportController.java
+++ src/main/java/pe/edu/utp/control_gastos_app/controller/ReportController.java
@@ -1,25 +1,31 @@
 package pe.edu.utp.control_gastos_app.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import pe.edu.utp.control_gastos_app.security.CustomUserDetails;
 import pe.edu.utp.control_gastos_app.service.report.ReportService;
 
 @Controller
 @RequestMapping("/reports")
 public class ReportController {
 
-    @Autowired
-    private ReportService reportService;
+    private final ReportService reportService;
+
+    public ReportController(ReportService reportService) {
+        this.reportService = reportService;
+    }
 
     @GetMapping("/financial-summary/pdf")
     public void downloadFinancialSummaryPdf(
-            HttpServletResponse response
-    ) throws Exception {
+            HttpServletResponse response,
+            @AuthenticationPrincipal UserDetails userDetails) throws Exception {
 
-        reportService.generateFinancialSummaryPdf(response);
+        Long userId = ((CustomUserDetails) userDetails).getId();
+        reportService.generateFinancialSummaryPdf(response, userId);
     }
 }
