a24c0a9774add6c05c6946ebe92608b6a1309bb1
Joseph Montoya <j.jhoel@outlook.es>
2026-05-23 23:06:36 -0500

    feat: Implementando CategoryExpenseDTO

--- src/main/java/pe/edu/utp/control_gastos_app/dto/report/CategoryExpenseDTO.java
+++ src/main/java/pe/edu/utp/control_gastos_app/dto/report/CategoryExpenseDTO.java
@@ -1,4 +1,14 @@
 package pe.edu.utp.control_gastos_app.dto.report;
 
 public class CategoryExpenseDTO {
+    private String categoryName;
+    private Double total;
+
+    public CategoryExpenseDTO(String categoryName, Double total) {
+        this.categoryName = categoryName;
+        this.total = total;
+    }
+
+    public String getCategoryName() { return categoryName; }
+    public Double getTotal() { return total; }
 }
