29e57e1f9f2083176a8d1beaa81667225f159393
Joseph Montoya <j.jhoel@outlook.es>
2026-05-23 23:30:44 -0500

    feat: Actualizando ReportService

--- src/main/java/pe/edu/utp/control_gastos_app/service/report/ReportService.java
+++ src/main/java/pe/edu/utp/control_gastos_app/service/report/ReportService.java
@@ -3,8 +3,5 @@ package pe.edu.utp.control_gastos_app.service.report;
 import jakarta.servlet.http.HttpServletResponse;
 
 public interface ReportService {
-
-    void generateFinancialSummaryPdf(
-            HttpServletResponse response
-    ) throws Exception;
+    void generateFinancialSummaryPdf(HttpServletResponse response, Long userId) throws Exception;
 }
