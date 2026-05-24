1e532c753d5ba872070912a85343bd76aef9ec6b
Joseph Montoya <j.jhoel@outlook.es>
2026-05-23 23:06:49 -0500

    feat: Implementando MonthlyExpenseDTO

--- src/main/java/pe/edu/utp/control_gastos_app/dto/report/MonthlyExpenseDTO.java
+++ src/main/java/pe/edu/utp/control_gastos_app/dto/report/MonthlyExpenseDTO.java
@@ -1,4 +1,17 @@
 package pe.edu.utp.control_gastos_app.dto.report;
 
 public class MonthlyExpenseDTO {
+    private String label; // "Ene 2025", "Feb 2025"...
+    private Double income;
+    private Double expense;
+
+    public MonthlyExpenseDTO(String label, Double income, Double expense) {
+        this.label = label;
+        this.income = income;
+        this.expense = expense;
+    }
+
+    public String getLabel() { return label; }
+    public Double getIncome() { return income; }
+    public Double getExpense() { return expense; }
 }
